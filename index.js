44d9c99123f515df7928b8089f304861b046efd3
jafar bino <binocoder@gmail.com>
2023-10-02 05:33:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
+function writeCards(names, event) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    thankYouMessages.push(thankYouMessage);
+  }
+
+  return thankYouMessages;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
